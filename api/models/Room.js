4b866198a5729749cf335b7752e142e4f063bb3a
Abhinav-Modi <abhinavmodi07@gmail.com>
2022-07-03 12:10:42 +0530

    CRUD

--- api/models/Room.js
+++ api/models/Room.js
@@ -1,6 +1,21 @@
-import mongoose from "mongoose";
-const { Schema } = mongoose;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const RoomSchema = new mongoose.Schema({
-	name: { type: String, required: true },
+const RoomSchema = new Schema({
+	title: { type: String, required: true },
+	price: { type: Number, required: true },
+	maxPeople: { type: Number, required: true },
+	desc: { type: String, required: true },
+	roomNumbers: [
+		{
+			number: Number,
+			unavailableDates:
+				// has all the dates in array for which the room is not booked
+				{ type: [Date] },
+		},
+	], //contains room numbers of all the rooms that fulfill the above conditions
 });
+
+const Room = mongoose.model("Room", RoomSchema);
+
+module.exports = Room;
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
 const router = express.Router();
+const roomController = require("../controllers/roomController");
+const verifyToken = require("../utils/verifyToken");
 
-router.get("/", (req, res) => {
-	res.send("rooms end");
-});
-
+// Create
+router.post("/:hotelid", verifyToken.adminVerify, roomController.createRoom);
+// Update
+router.put("/:id", verifyToken.adminVerify, roomController.updatedRoom);
+// Delete
+router.delete(
+	"/:id/:hotelid",
+	verifyToken.adminVerify,
+	roomController.deleteRoom
+);
+// Get
+router.get("/:id", roomController.getRoom);
+router.get("/", roomController.getAllRoom);
 module.exports = router;
